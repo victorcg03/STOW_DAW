92c7de9939400a1ba85d4ccfcec4fcbbcecfb527
victorcg03 <78515761+victorcg03@users.noreply.github.com>
2024-12-13 10:20:00 +0100

    Header

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include_once("./partials/header.php") ?>
+<link rel="stylesheet" href="./css/index.css">
+<main>
+    <div class="banner">
+
+    </div>
+</main>
+
---
+++ partials/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>STOW</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/cade5ed75a.js" crossorigin="anonymous"></script>
+    <script src="./js/header.js"></script>
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/reset.css">
+</head>
+<body>   
+    <header>
+        <i class="fa-solid fa-bars"></i>
+        <div class="central">
+            <a href="./productos.php">Productos</a>
+            <a href="./">STOW</a>
+            <a href="nosotros.php">Sobre nosotros</a>
+        </div>
+        <nav>
+            <i class="fa-solid fa-user"></i>
+            <i class="fa-solid fa-search"></i>
+            <i class="fa-solid fa-shopping-cart"></i>
+        </nav>
+    </header>
