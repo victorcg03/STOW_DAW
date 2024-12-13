6da54fb730ea826d1069929df9ee5d4d46fe4811
victorcg03 <victorjosecorralguillot@gmail.com>
2024-12-13 19:16:57 +0100

    Header Menus

--- js/header.js
+++ js/header.js
@@ -1,22 +1,19 @@
-window.addEventListener('load', ()=>{
-    document.querySelector('.fa-bars').addEventListener("click", ()=>{
-        esconderMenus("menu-izquierdo");
-        document.querySelector('.menu-izquierdo').classList.toggle('visible');
-    });
-    document.querySelector('.fa-shopping-cart').addEventListener("click", ()=>{
-        esconderMenus("carrito");
-        document.querySelector('.carrito').classList.toggle('visible');
-    });
-    document.querySelector('.fa-search').addEventListener("click", ()=>{
-        esconderMenus("busqueda");
-        document.querySelector('.busqueda').classList.toggle('visible');
-    });
-    document.querySelector("main").addEventListener("click", esconderMenus);
-});
-function esconderMenus(menu){
-    document.querySelectorAll(".visible").forEach(activo =>{
-        if (!activo.classList.contains(menu)) {
-            activo.classList.remove("visible");
+window.addEventListener('load', () => {
+    document.querySelector("#google_translate_element .skiptranslate").childNodes.forEach(nodo => {
+        if (nodo.nodeName === "#text" || nodo.nodeName === "SPAN") {
+            nodo.remove();
         }
     });
-}
+    document.querySelectorAll("header i").forEach(el => el.addEventListener("click", ()=>{
+        let visible = document.querySelector(`.visible:not(.${el.dataset.menu})`);       
+        if (visible) {
+            visible.classList.remove("visible");
+        }
+        setTimeout(() => {
+            document.querySelector(`.${el.dataset.menu}`).classList.toggle("visible");
+        }, visible ? 400 : 0);
+    }));
+    document.querySelector("main").addEventListener("click", ()=>{
+        document.querySelector(".visible").classList.remove("visible");
+    });
+});
--- partials/header.php
+++ partials/header.php
@@ -1,10 +1,13 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <title>STOW</title>
+    <title class="notranslate">STOW</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
@@ -12,57 +15,82 @@
     <script src="../js/header.js"></script>
     <link rel="stylesheet" href="./css/header.css">
     <link rel="stylesheet" href="./css/reset.css">
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en',
+                includedLanguages: 'en,es,fr,de,it'
+            }, 'google_translate_element');
+        }
+    </script>
+    <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
 </head>
-<body>   
+
+<body>
     <header>
-        <i class="fa-solid fa-bars"></i>
+        <i class="fa-solid fa-bars" data-menu="menu-izquierdo"></i>
         <div class="central">
-            <a href="./productos.php">Productos</a>
-            <a href="./">STOW</a>
-            <a href="nosotros.php">Sobre nosotros</a>
+            <a class="notranslate" href="./">STOW</a>
         </div>
         <nav>
-            <i class="fa-solid fa-user"></i>
-            <i class="fa-solid fa-search"></i>
-            <i class="fa-solid fa-shopping-cart"></i>
+            <i class="fa-solid fa-user" data-menu="usuario"></i>
+            <i class="fa-solid fa-search" data-menu="busqueda"></i>
+            <i class="fa-solid fa-shopping-cart" data-menu="carrito"></i>
         </nav>
         <div class="menu-izquierdo">
             <ul>
+                <li><a href="./productos.php">Productos</a></li>
                 <li>
-                    <a href="./productos.php/?sexo=hombre">Hombre</a>
                     <ul>
-                        <li><a href="./productos.php/?sexo=hombre&tipo=sudaderas">Sudaderas</a></li>
-                        <li><a href="./productos.php/?sexo=hombre&tipo=camisetas">Camisetas</a></li>
-                        <li><a href="./productos.php/?sexo=hombre&tipo=gorros">Gorros</a></li>
+                        <li>
+                            <a href="./productos.php/?sexo=hombre">Hombre</a>
+                            <ul>
+                                <li><a href="./productos.php/?sexo=hombre&tipo=sudaderas">Sudaderas</a></li>
+                                <li><a href="./productos.php/?sexo=hombre&tipo=camisetas">Camisetas</a></li>
+                                <li><a href="./productos.php/?sexo=hombre&tipo=gorros">Gorros</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="./productos.php/?sexo=mujer">Mujer</a>
+                            <ul>
+                                <li><a href="./productos.php/?sexo=mujer&tipo=sudaderas">Sudaderas</a></li>
+                                <li><a href="./productos.php/?sexo=mujer&tipo=camisetas">Camisetas</a></li>
+                                <li><a href="./productos.php/?sexo=mujer&tipo=gorros">Gorros</a></li>
+                            </ul>
+                        </li>
                     </ul>
                 </li>
                 <li>
-                    <a href="./productos.php/?sexo=mujer">Mujer</a>
-                    <ul>
-                        <li><a href="./productos.php/?sexo=mujer&tipo=sudaderas">Sudaderas</a></li>
-                        <li><a href="./productos.php/?sexo=mujer&tipo=camisetas">Camisetas</a></li>
-                        <li><a href="./productos.php/?sexo=mujer&tipo=gorros">Gorros</a></li>
-                    </ul>
+                    <label>Idioma</label>
+                    <div id="google_translate_element"></div>
                 </li>
                 <li>
-                    <label for="idioma">Idioma</label> <select name="idioma" id="idioma">
-                        <option value="es">Español</option>
-                        <option value="en">Ingles</option>
-                    </select>
+                    <a href="./contacto.php">Contacto</a>
                 </li>
                 <li>
-                    <a href=".contacto.php">Contacto</a>
+                    <a href="./nosotros.php">Sobre nosotros</a>
                 </li>
                 <li>
-                    <a href="./ayuda.php">Ayuda</a>
+                    <a href="./opiniones.php">Opiniones</a>
                 </li>
-                <li><a href="./opiniones.php">Opiniones</a></li>
             </ul>
         </div>
         <div class="carrito">
-            
+
         </div>
         <div class="busqueda">
             <input type="search" name="search" id="search" placeholder="Buscar...">
+        </div>
+        <div class="usuario">
+            <?php
+            if (empty($_SESSION['user'])) { ?>
+                <div>
+                    <a href="./login.php">Iniciar sesión</a>
+                    <p>o</p>
+                    <a href="./register.php">Registrarse</a>
+                </div>
+            <?php  }
+            ?>
         </div>
     </header>
