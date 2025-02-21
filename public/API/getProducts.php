9cb9aa5afd45f2b81ce0a8b30d6663fbf255fbe3
victorcg03 <victorjosecorralguillot@gmail.com>
2025-02-21 21:29:26 +0100

    API FIXES

--- public/API/getProducts.php
+++ public/API/getProducts.php
@@ -1,13 +1,13 @@
 <?php
     require_once '../database.php';
     if ($_GET["id"]) {
-        $statement = $conne->prepare("SELECT * FROM products WHERE id = :id WHERE activo = 1");
+        $statement = $conne->prepare("SELECT * FROM productos WHERE id = :id WHERE activo = 1");
         $statement->bindParam(':id', $_GET["id"]);
         $statement->execute();
         $result = $statement->fetch(PDO::FETCH_ASSOC);
         echo json_encode($result);
     } else {
-        $statement = $conne->prepare("SELECT * FROM products WHERE activo = 1");
+        $statement = $conne->prepare("SELECT * FROM productos WHERE activo = 1");
         $statement->execute();
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($result);
