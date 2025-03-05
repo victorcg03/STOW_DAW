f1a84f59bfa63d992522775c7dbdc5e9d7228e4e
victorcg03 <victorjosecorralguillot@gmail.com>
2025-03-05 15:18:17 +0100

    .

--- public/API/getProducts.php
+++ public/API/getProducts.php
@@ -1,15 +1,11 @@
 <?php
 require_once 'config.php';
-header("Access-Control-Allow-Origin: *");  // Permite solicitudes de cualquier origen
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");  // Métodos permitidos
-header("Access-Control-Allow-Headers: Content-Type, Authorization");  // Cabeceras permitidas
-
 try {
     if (isset($_GET["id"])) {
         $statement = $pdo->prepare("SELECT * FROM productos WHERE id = :id AND activo = 1");
         $statement->bindParam(':id', $_GET["id"], PDO::PARAM_INT);
     } else {
-        $statement = $conne->prepare("SELECT * FROM productos WHERE activo = 1");
+        $statement = $pdo->prepare("SELECT * FROM productos WHERE activo = 1");
     }
 
     $statement->execute();
