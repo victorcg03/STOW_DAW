905d8a5a38409090a26befc3d7054b698b99dea9
victorcg03 <victorjosecorralguillot@gmail.com>
2025-03-06 00:00:55 +0100

    Agregar manejo de "me gusta" en la API con soporte para insertar y eliminar registros

---
+++ public/API/handleLike.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'jwt_middleware.php';
+require_once 'config.php';
+$decoded = verificarToken();
+$like = null;
+$stmt = $pdo->prepare("SELECT ProductoID FROM likes WHERE Usuario = ? AND ProductoID = ?");
+$stmt->execute([$decoded->correo, $_POST['ID']]);
+$like = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($stmt->rowCount() == 0) {
+  $stmt = $pdo->prepare("INSERT INTO likes (Usuario, ProductoID) VALUES (?, ?)");
+  $stmt->execute([$decoded->correo, $_POST['ID']]);
+  echo json_encode(["ok" => "ok"]);
+} else {
+  $stmt = $pdo->prepare("DELETE FROM likes WHERE Usuario = ? AND ProductoID = ?");
+  $stmt->execute([$decoded->correo, $_POST['ID']]);
+  echo json_encode(["ok" => "ok"]);
+}
