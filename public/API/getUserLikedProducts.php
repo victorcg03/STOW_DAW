b3abba6d0bc983337b9630e9f1b77f1338f7f4f3
victorcg03 <victorjosecorralguillot@gmail.com>
2025-03-06 00:55:07 +0100

    .

--- public/API/getUserLikedProducts.php
+++ public/API/getUserLikedProducts.php
@@ -2,4 +2,15 @@
 require_once 'jwt_middleware.php';
 require_once 'config.php';
 $decoded = verificarToken();
-echo json_encode(["message" => "Hola, " . $decoded->correo]);
+
+// Lee los datos JSON del cuerpo de la solicitud
+try {
+  $stmt = $pdo->prepare("SELECT ProductoID FROM likes WHERE Usuario = ? AND activo = 1");
+  $stmt->execute([$decoded->correo]);
+  $likes = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  echo json_encode($likes);
+} catch (Exception $e) {
+  echo json_encode(array("message" => $e->getMessage()));
+  die();
+}
