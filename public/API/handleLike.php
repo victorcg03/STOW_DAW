f1e818fc7ffdecd235ba05b53ede21797e93014c
victorcg03 <victorjosecorralguillot@gmail.com>
2025-03-06 00:41:18 +0100

    .

--- public/API/handleLike.php
+++ public/API/handleLike.php
@@ -6,4 +6,8 @@ $decoded = verificarToken();
 // Lee los datos JSON del cuerpo de la solicitud
 $data = json_decode(file_get_contents("php://input"), true);
 
-echo json_encode($data);
+$like = null;
+$stmt = $pdo->prepare("SELECT ProductoID FROM likes WHERE Usuario = ? AND ProductoID = ?");
+$stmt->execute([$decoded->correo, $data['ID']]);
+$like = $stmt->fetch(PDO::FETCH_ASSOC);
+echo json_encode($like);
