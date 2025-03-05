8bdac5fffa2a1730604c51a4100538c6aa7e909a
victorcg03 <victorjosecorralguillot@gmail.com>
2025-03-06 00:38:59 +0100

    .

--- public/API/handleLike.php
+++ public/API/handleLike.php
@@ -6,17 +6,4 @@ $decoded = verificarToken();
 // Lee los datos JSON del cuerpo de la solicitud
 $data = json_decode(file_get_contents("php://input"), true);
 
-$like = null;
-$stmt = $pdo->prepare("SELECT ProductoID FROM likes WHERE Usuario = ? AND ProductoID = ?");
-$stmt->execute([$decoded->correo, $data['ID']]); // Usa $data['ID'] en lugar de $_POST['ID']
-$like = $stmt->fetch(PDO::FETCH_ASSOC);
-
-if ($stmt->rowCount() == 0) {
-  $stmt = $pdo->prepare("INSERT INTO likes (Usuario, ProductoID) VALUES (?, ?)");
-  $stmt->execute([$decoded->correo, $data['ID']]);
-  echo json_encode(["ok" => "ok"]);
-} else {
-  $stmt = $pdo->prepare("DELETE FROM likes WHERE Usuario = ? AND ProductoID = ?");
-  $stmt->execute([$decoded->correo, $data['ID']]);
-  echo json_encode(["ok" => "okk"]);
-}
+echo json_encode($data);
