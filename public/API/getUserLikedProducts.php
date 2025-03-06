70420768480dd3eb71f8ea40489fcaa9d7045a4e
victorcg03 <victorjosecorralguillot@gmail.com>
2025-03-06 01:12:18 +0100

    .

--- public/API/getUserLikedProducts.php
+++ public/API/getUserLikedProducts.php
@@ -7,7 +7,7 @@ $decoded = verificarToken();
 try {
   $stmt = $pdo->prepare("SELECT ProductoID FROM likes WHERE Usuario = ?");
   $stmt->execute([$decoded->correo]);
-  $likes = $stmt->fetchAll();
+  $likes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
   echo json_encode($likes);
 } catch (Exception $e) {
